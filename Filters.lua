83a4d328b93988034ed29f4fccf9b95b79d68c2e
aelobin <28518245+Aelobin@users.noreply.github.com>
2011-10-09 22:40:12 +0000

    /

--- Filters.lua
+++ Filters.lua
@@ -2,3 +2,15 @@ local MogIt,mog = ...;
 local L = mog.L;
 
 mog.filt = CreateFrame("Frame","MogItFilters",mog.frame);
+
+mog.filters = {};
+
+function mog:AddFilter(name,frame)
+	if mog.filters[name] then return end;
+	frame = frame or CreateFrame("Frame");
+	frame:Hide();
+	frame:SetParent(mog.filt);
+	frame.data = {};
+	mog.filters[name] = frame;
+	return frame;
+end
