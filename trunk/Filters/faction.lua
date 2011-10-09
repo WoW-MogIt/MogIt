5026c529f8af226e0532f94a1138230bec241472
aelobin <aelobin@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2011-10-09 22:35:54 +0000

    /

---
+++ trunk/Filters/faction.lua
@@ -0,0 +1,52 @@
+local MogIt,mog = ...;
+local L = mog.L;
+
+local f = mog:AddFilter("faction");
+local alliance;
+local horde;
+
+f.faction = f:CreateFontString(nil,"ARTWORK","GameFontHighlightSmall");
+f.faction:SetPoint("TOPLEFT",f,"TOPLEFT",0,0);
+f.faction:SetPoint("RIGHT",f,"RIGHT",0,0);
+f.faction:SetText(L["Faction"]..":");
+f.faction:SetJustifyH("LEFT");
+
+f.alliance = CreateFrame("CheckButton","MogItFiltersFactionAlliance",f,"UICheckButtonTemplate");
+MogItFiltersFactionAllianceText:SetText(FACTION_ALLIANCE);
+f.alliance:SetPoint("TOPLEFT",f.faction,"BOTTOMLEFT",0,0);
+f.alliance:SetScript("OnClick",function(self)
+	alliance = self:GetChecked();
+	if f.data.Alliance then
+		f.data.Alliance(f.module,self,f);
+	end
+end);
+
+f.horde = CreateFrame("CheckButton","MogItFiltersFactionHorde",f,"UICheckButtonTemplate");
+MogItFiltersFactionHordeText:SetText(FACTION_HORDE);
+f.horde:SetPoint("TOPLEFT",f.alliance,"BOTTOMLEFT",0,0);
+f.horde:SetScript("OnClick",function(self)
+	horde = self:GetChecked();
+	if f.data.Horde then
+		f.data.Horde(f.module,self,f);
+	end
+end);
+
+f:SetScript("OnShow",function(self)
+	if f.data.OnShow then
+		f.data.OnShow(f.module,f);
+	end
+end);
+
+function f.Filter(faction,a,h)
+	a = a or alliance;
+	h = h or horde;
+	return (not faction) or (faction == 1 and a) or (faction == 2 and h);
+end
+
+function f.Default()
+	alliance = UnitFactionGroup("PLAYER") == "Alliance";
+	f.alliance:SetChecked(alliance);
+	horde = UnitFactionGroup("PLAYER") == "Horde";
+	f.horde:SetChecked(horde);
+end
+f.Default();
---
+++ trunk/Filters/slot.lua
@@ -0,0 +1,86 @@
+local MogIt,mog = ...;
+local L = mog.L;
+
+local f = mog:AddFilter("slot");
+local selected;
+local num;
+local all;
+
+f.slot = f:CreateFontString(nil,"ARTWORK","GameFontHighlightSmall");
+f.slot:SetPoint("TOPLEFT",f,"TOPLEFT",0,0);
+f.slot:SetPoint("RIGHT",f,"RIGHT",0,0);
+f.slot:SetText(L["Slot"]..":");
+f.slot:SetJustifyH("LEFT");
+
+f.dd = CreateFrame("Frame","MogItFiltersSlotDropdown",f,"UIDropDownMenuTemplate");
+f.dd:SetPoint("TOPLEFT",f.slot,"BOTTOMLEFT",-16,-2);
+UIDropDownMenu_SetWidth(f.dd,125);
+UIDropDownMenu_SetButtonWidth(f.dd,140);
+UIDropDownMenu_JustifyText(f.dd,"LEFT");
+function f.dd.initialize(self)
+	local info;
+		info = UIDropDownMenu_CreateInfo();
+		info.text =	all and L["Select All"] or L["Select None"];
+		info.func = function(self)
+			num = 0;
+			for k,v in ipairs(mog.sub.slots) do
+				selected[k] = all;
+				num = num + (all and 1 or 0);
+			end
+			all = not all;
+			UIDropDownMenu_SetText(f.dd,L["%d selected"]:format(num));
+			ToggleDropDownMenu(1,nil,f.dd);
+			if f.data.Dropdown then
+				f.data.Dropdown(f.module,self,f);
+			end
+		end
+		info.notCheckable = true;
+		UIDropDownMenu_AddButton(info);
+		
+		for k,v in ipairs(mog.sub.slots) do
+			info = UIDropDownMenu_CreateInfo();
+			info.text =	v;
+			info.value = k;
+			info.func = function(self)
+				if selected[self.value] and (not self.checked) then
+					num = num - 1;
+				elseif (not selected[self.value]) and self.checked then
+					num = num + 1;
+				end
+				selected[self.value] = self.checked;
+				UIDropDownMenu_SetText(f.dd,L["%d selected"]:format(num));
+				if f.data.Dropdown then
+					f.data.Dropdown(f.module,self,f);
+				end
+			end
+			info.keepShownOnClick = true;
+			info.isNotRadio = true;
+			info.checked = selected[k];
+			UIDropDownMenu_AddButton(info);
+		end
+	end
+end
+
+f:SetScript("OnShow",function(self)
+	if f.data.OnShow then
+		f.data.OnShow(f.module,f);
+	end
+end);
+
+function f.Filter(slot1)
+	return (not slot1) or selected[slot1];
+end
+
+function f.Default()
+	selected = {};
+	num = 0;
+	all = nil;
+	
+	for k,v in ipairs(mog.sub.slots) do
+		selected[k] = true;
+		num = num + 1;
+	end
+
+	UIDropDownMenu_SetText(f.dd,L["%d selected"]:format(num));
+end
+f.Default();
