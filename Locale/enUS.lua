58f23ec4a7bf330ab1d3a0f1a29de1809c258047
aelobin <28518245+Aelobin@users.noreply.github.com>
2011-10-11 17:08:59 +0000

    /

--- Locale/enUS.lua
+++ Locale/enUS.lua
@@ -1,9 +1,5 @@
-local MogIt,mog = ...;
-mog.L = setmetatable({},{__index = function(tbl,key)
-	return key;
-end});
-
 --[[
 if GetLocale() ~= "enUS" and GetLocale() ~= "enGB" then return end;
+local MogIt,mog = ...;
 local L = mog.L;
 --]]
