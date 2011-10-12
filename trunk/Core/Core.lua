64f37aea4a17b551da4e801dfa5d042ca04d805e
aelobin <aelobin@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2011-10-12 14:47:04 +0000

    /

--- trunk/Core/Core.lua
+++ trunk/Core/Core.lua
@@ -27,7 +27,7 @@ mog.face = 0;
 -- Sort/Filter/URL
 
 function mog:RegisterModule(name,data,base)
-	if mog.modules.lookup[name] then return end;
+	if mog.modules.lookup[name] then return mog.modules.lookup[name] end;
 	mog.modules.lookup[name] = data;
 	table.insert(base and mog.modules.base or mog.modules.extra,data);
 	if UIDropDownMenu_GetCurrentDropDown() == mog.dropdown and DropDownList1 and DropDownList1:IsShown() then
