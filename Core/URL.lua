618055d77269d1a049db3a1a2a5adbd176d97a9e
aelobin <28518245+Aelobin@users.noreply.github.com>
2011-10-19 21:20:19 +0000

    /

--- Core/URL.lua
+++ Core/URL.lua
@@ -1,13 +1,21 @@
 local MogIt,mog = ...;
 local L = mog.L;
 
-local sites = {};
+mog.url = {};
 
-function mog:ShowURL(id,sub,url)
+function mog:AddURL(name,tbl)
+	mog.url[name] = tbl;
+end
+
+function mog:ShowURL(id,sub,url,force)
+	if not id then return end;
 	url = url or mog.db.profile.url;
 	sub = sub or "item";
-	if sites[url] and sites[url][sub] then
-		StaticPopup_Show("MOGIT_URL",sites[url].fav and "\124TInterface\\AddOns\\MogIt\\Images\\"..sites[url].fav..":18:18\124t " or "",url,sites[url][sub]:format(id));
+	if not (force or (mog.url[url] and mog.url[url][sub])) then
+		url = "Wowhead";
+	end
+	if mog.url[url] and mog.url[url][sub] then
+		StaticPopup_Show("MOGIT_URL",mog.url[url].fav and "\124T"..mog.url[url].fav..":18:18\124t " or "",url,mog.url[url][sub]:format(id));
 		return true;
 	end
 end
@@ -35,61 +43,54 @@ StaticPopupDialogs["MOGIT_URL"] = {
 	hideOnEscape = 1
 };
 
-sites["Battle.net"] = {
-	fav = "fav_wow",
-	--url = L["http://eu.battle.net/wow/en/"],
+mog:AddURL("Battle.net",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_wow",
 	item = L["http://eu.battle.net/wow/en/"].."item/%d",
-};
+});
 
-sites["Wowhead"] = {
-	fav = "fav_wh",
-	--url = L["http://www.wowhead.com/"],
+mog:AddURL("Wowhead",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_wh",
 	item = L["http://www.wowhead.com/"].."item=%d",
 	set = L["http://www.wowhead.com/"].."itemset=%d",
 	npc = L["http://www.wowhead.com/"].."npc=%d",
 	spell = L["http://www.wowhead.com/"].."spell=%d",
-};
+});
 
-sites["MMO-Champion"] = {
-	fav = "fav_mmo",
-	--url = "http://db.mmo-champion.com/",
+mog:AddURL("MMO-Champion",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_mmo",
 	item = "http://db.mmo-champion.com/i/%d/",
 	set = "http://db.mmo-champion.com/is/%d/",
 	npc = "http://db.mmo-champion.com/c/%d/",
 	spell = "http://db.mmo-champion.com/s/%d/",
-};
+});
 
-sites["Wowpedia"] = {
-	fav = "fav_wp",
-	--url = "http://www.wowpedia.org/"
+mog:AddURL("Wowpedia",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_wp",
 	item = "http://www.wowpedia.org/index.php?search=\"{{elinks-item|%d}}\"",
 	set = "http://www.wowpedia.org/index.php?search=\"{{elinks-set|%d}}\"",
 	npc = "http://www.wowpedia.org/index.php?search=\"{{elinks-NPC|%d}}\"",
 	spell = "http://www.wowpedia.org/index.php?search=\"{{elinks-spell|%d}}\"",
-};
+});
 
-sites["Thottbot"] = {
-	fav = "fav_tb",
-	--url = "http://thottbot.com/"
+mog:AddURL("Thottbot",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_tb",
 	item = "http://thottbot.com/item=%d",
 	set = "http://thottbot.com/itemset=%d",
 	npc = "http://thottbot.com/npc=%d",
 	spell = "http://thottbot.com/spell=%d",
-};
+});
 
-sites["Buffed.de"] = {
-	fav = "fav_buff",
-	--url = "http://wowdata.buffed.de/"
+mog:AddURL("Buffed.de",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_buff",
 	item = "http://wowdata.buffed.de/?i=%d",
 	set = "http://wowdata.buffed.de/?set=%d",
 	npc = "http://wowdata.buffed.de/?n=%d",
 	spell = "http://wowdata.buffed.de/?s=%d",
-};
+});
 
-sites["JudgeHype"] = {
-	fav = "fav_jh",
-	--url = "http://worldofwarcraft.judgehype.com/"
+mog:AddURL("JudgeHype",{
+	fav = "Interface\\AddOns\\MogIt\\Images\\fav_jh",
 	item = "http://worldofwarcraft.judgehype.com/?page=objet&w=%d",
 	npc = "http://worldofwarcraft.judgehype.com/index.php?page=pnj&w=%d",
 	spell = "http://worldofwarcraft.judgehype.com/index.php?page=spell&w=%d",
-};
+});
