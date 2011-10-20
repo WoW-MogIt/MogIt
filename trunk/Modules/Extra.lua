5660b7fddea1c52aecb19d97170c396f4ab0df5f
aelobin <aelobin@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2011-10-20 01:53:59 +0000

    /

--- trunk/Modules/Extra.lua
+++ trunk/Modules/Extra.lua
@@ -1,6 +1,12 @@
 local MogIt,mog = ...;
 local L = mog.L;
 
+local function tempOnClick(self)
+	if not self.value.loaded then
+		LoadAddOn(self.value.addon);
+	end
+end
+
 local function temp(module,tier)
 	local info;
 	if tier == 1 then
@@ -10,30 +16,14 @@ local function temp(module,tier)
 		info.colorCode = "\124cFF"..(module.loaded and "00FF00" or "FF0000");
 		info.keepShownOnClick = true;
 		info.notCheckable = true;
-		info.func = function(self)
-			if not self.value.loaded then
-				LoadAddOn(self.value.addon);
-			end
-		end
+		info.func = tempOnClick;
 		UIDropDownMenu_AddButton(info,tier);
 	end
 end
 
-local base = {
-	MogIt_Cloth = true,
-	MogIt_Leather = true,
-	MogIt_Mail = true,
-	MogIt_Plate = true,
-	MogIt_OneHanded = true,
-	MogIt_TwoHanded = true,
-	MogIt_Ranged = true,
-	MogIt_Other = true,
-	MogIt_Accessories = true,
-};
-
 for i=1,GetNumAddOns() do
 	local name,title,_,_,loadable = GetAddOnInfo(i);
-	if loadable and (not base[name]) and name:find("^MogIt_") then
+	if loadable and (not mog:GetModule(name)) and name:find("^MogIt_") then
 		mog:RegisterModule(name,{
 			name = title:match("^MogIt_(.+)") or title,
 			Dropdown = temp,
