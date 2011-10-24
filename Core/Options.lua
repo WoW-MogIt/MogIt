ac0f77974096ab7d3613f08220e110e43b0ea144
lombra <Lombra@users.noreply.github.com>
2011-10-24 14:00:37 +0000

    - Go to config using the options button. - Options optimisations.

--- Core/Options.lua
+++ Core/Options.lua
@@ -1,15 +1,12 @@
-﻿local MogIt,mog = ...;
-local L = mog.L;
+﻿local MogIt, mog = ...
+local L = mog.L
 --[[
 local L = {
 	default = "Default",
 	reset = "Reset profile",
-	choose_desc = "You can either create a new profile by entering a name in the editbox, or choose one of the already existing profiles.",
 	new = "New",
 	choose = "Existing profiles",
-	copy_desc = "Copy the settings from one existing profile into the currently active profile.",
 	copy = "Copy From",
-	delete_desc = "Delete existing and unused profiles from the database to save space, and cleanup the SavedVariables file.",
 	delete = "Delete a profile",
 	delete_confirm = "Are you sure you want to delete the selected profile?",
 	profiles = "Profiles",
@@ -20,12 +17,9 @@ local LOCALE = GetLocale()
 if LOCALE == "deDE" then
 	L["default"] = "Standard"
 	L["reset"] = "Profil zur\195\188cksetzen"
-	L["choose_desc"] = "Du kannst ein neues Profil erstellen, indem du einen neuen Namen in der Eingabebox 'Neu' eingibst, oder w\195\164hle eines der vorhandenen Profile aus."
 	L["new"] = "Neu"
 	L["choose"] = "Vorhandene Profile"
-	L["copy_desc"] = "Kopiere die Einstellungen von einem vorhandenen Profil in das aktive Profil."
 	L["copy"] = "Kopieren von..."
-	L["delete_desc"] = "L\195\182sche vorhandene oder unbenutzte Profile aus der Datenbank um Platz zu sparen und um die SavedVariables Datei 'sauber' zu halten."
 	L["delete"] = "Profil l\195\182schen"
 	L["delete_confirm"] = "Willst du das ausgew\195\164hlte Profil wirklich l\195\182schen?"
 	L["profiles"] = "Profile"
@@ -33,24 +27,18 @@ if LOCALE == "deDE" then
 elseif LOCALE == "frFR" then
 	L["default"] = "D\195\169faut"
 	L["reset"] = "R\195\169initialiser le profil"
-	L["choose_desc"] = "Vous pouvez cr\195\169er un nouveau profil en entrant un nouveau nom dans la bo\195\174te de saisie, ou en choississant un des profils d\195\169j\195\160 existants."
 	L["new"] = "Nouveau"
 	L["choose"] = "Profils existants"
-	L["copy_desc"] = "Copie les param\195\168tres d'un profil d\195\169j\195\160 existant dans le profil actuellement actif."
 	L["copy"] = "Copier \195\160 partir de"
-	L["delete_desc"] = "Supprime les profils existants inutilis\195\169s de la base de donn\195\169es afin de gagner de la place et de nettoyer le fichier SavedVariables."
 	L["delete"] = "Supprimer un profil"
 	L["delete_confirm"] = "Etes-vous s\195\187r de vouloir supprimer le profil s\195\169lectionn\195\169 ?"
 	L["profiles"] = "Profils"
 elseif LOCALE == "koKR" then
 	L["default"] = "기본값"
 	L["reset"] = "프로필 초기화"
-	L["choose_desc"] = "새로운 이름을 입력하거나, 이미 있는 프로필중 하나를 선택하여 새로운 프로필을 만들 수 있습니다."
 	L["new"] = "새로운 프로필"
 	L["choose"] = "프로필 선택"
-	L["copy_desc"] = "현재 사용중인 프로필에, 선택한 프로필의 설정을 복사합니다."
 	L["copy"] = "복사"
-	L["delete_desc"] = "데이터베이스에 사용중이거나 저장된 프로파일 삭제로 SavedVariables 파일의 정리와 공간 절약이 됩니다."
 	L["delete"] = "프로필 삭제"
 	L["delete_confirm"] = "정말로 선택한 프로필의 삭제를 원하십니까?"
 	L["profiles"] = "프로필"
@@ -58,12 +46,9 @@ elseif LOCALE == "koKR" then
 elseif LOCALE == "esES" or LOCALE == "esMX" then
 	L["default"] = "Por defecto"
 	L["reset"] = "Reiniciar Perfil"
-	L["choose_desc"] = "Puedes crear un nuevo perfil introduciendo un nombre en el recuadro o puedes seleccionar un perfil de los ya existentes."
 	L["new"] = "Nuevo"
 	L["choose"] = "Perfiles existentes"
-	L["copy_desc"] = "Copia los ajustes de un perfil existente al perfil actual."
 	L["copy"] = "Copiar de"
-	L["delete_desc"] = "Borra los perfiles existentes y sin uso de la base de datos para ganar espacio y limpiar el archivo SavedVariables."
 	L["delete"] = "Borrar un Perfil"
 	L["delete_confirm"] = "¿Estas seguro que quieres borrar el perfil seleccionado?"
 	L["profiles"] = "Perfiles"
@@ -71,12 +56,9 @@ elseif LOCALE == "esES" or LOCALE == "esMX" then
 elseif LOCALE == "zhTW" then
 	L["default"] = "預設"
 	L["reset"] = "重置設定檔"
-	L["choose_desc"] = "你可以通過在文本框內輸入一個名字創立一個新的設定檔，也可以選擇一個已經存在的設定檔。"
 	L["new"] = "新建"
 	L["choose"] = "現有的設定檔"
-	L["copy_desc"] = "從當前某個已保存的設定檔複製到當前正使用的設定檔。"
 	L["copy"] = "複製自"
-	L["delete_desc"] = "從資料庫裏刪除不再使用的設定檔，以節省空間，並且清理SavedVariables檔。"
 	L["delete"] = "刪除一個設定檔"
 	L["delete_confirm"] = "你確定要刪除所選擇的設定檔嗎？"
 	L["profiles"] = "設定檔"
@@ -84,12 +66,9 @@ elseif LOCALE == "zhTW" then
 elseif LOCALE == "zhCN" then
 	L["default"] = "默认"
 	L["reset"] = "重置配置文件"
-	L["choose_desc"] = "你可以通过在文本框内输入一个名字创立一个新的配置文件，也可以选择一个已经存在的配置文件。"
 	L["new"] = "新建"
 	L["choose"] = "现有的配置文件"
-	L["copy_desc"] = "从当前某个已保存的配置文件复制到当前正使用的配置文件。"
 	L["copy"] = "复制自"
-	L["delete_desc"] = "从数据库里删除不再使用的配置文件，以节省空间，并且清理SavedVariables文件。"
 	L["delete"] = "删除一个配置文件"
 	L["delete_confirm"] = "你确定要删除所选择的配置文件么？"
 	L["profiles"] = "配置文件"
@@ -97,12 +76,9 @@ elseif LOCALE == "zhCN" then
 elseif LOCALE == "ruRU" then
 	L["default"] = "По умолчанию"
 	L["reset"] = "Сброс профиля"
-	L["choose_desc"] = "Вы можете создать новый профиль, введя название в поле ввода, или выбрать один из уже существующих профилей."
 	L["new"] = "Новый"
 	L["choose"] = "Существующие профили"
-	L["copy_desc"] = "Скопировать настройки из выбранного профиля в активный."
 	L["copy"] = "Скопировать из"
-	L["delete_desc"] = "Удалить существующий и неиспользуемый профиль из БД для сохранения места, и очистить SavedVariables файл."
 	L["delete"] = "Удалить профиль"
 	L["delete_confirm"] = "Вы уверены, что вы хотите удалить выбранный профиль?"
 	L["profiles"] = "Профили"
@@ -200,6 +176,7 @@ end
 local frame = CreateFrame("Frame")
 frame.name = MogIt
 InterfaceOptions_AddCategory(frame)
+mog.config = frame
 
 local options = {
 	{
@@ -256,25 +233,24 @@ local options = {
 		type = "select",
 		text = L["Item URL"],
 		setting = "url",
-		menu = (function()
-			local function onClick(self)
-				mog.db.profile.url = self.value
-				self.owner:SetSelectedValue(self.value)
-			end
+		initialize = function(self, level)
 			local menu = {}
 			for i, v in ipairs(mog.sortedURLs) do
 				local data = mog.url[v]
-				local info = {
-					text = "\124T"..data.fav..":18:18\124t "..v,
-					value = v,
-					func = onClick,
-					notCheckable = true,
-					-- icon = "Interface\\AddOns\\MogIt\\Images\\"..mog.urlFav[v],
-				}
-				tinsert(menu, info)
+				local info = UIDropDownMenu_CreateInfo()
+				info.text = "\124T"..data.fav..":18:18\124t "..v
+				info.value = v
+				info.func = self.func
+				info.notCheckable = true
+				info.owner = self
+				-- info.icon = "Interface\\AddOns\\MogIt\\Images\\"..mog.urlFav[v]
+				UIDropDownMenu_AddButton(info, level)
 			end
-			return menu
-		end)(),
+		end,
+		onClick = function(self)
+			mog.db.profile.url = self.value
+			self.owner:SetSelectedValue(self.value)
+		end,
 	},
 	-- {
 		--tooltipWidth = 300,
@@ -318,6 +294,8 @@ for i, v in ipairs(options) do
 	local widget
 	if v.type == "select" then
 		widget = createDropDownMenu("MogIt"..v.setting.."Dropdown", frame, v.menu)
+		widget.initialize = v.initialize or widget.initialize
+		widget.func = v.onClick
 		widget.label:SetText(v.text)
 		
 		widget.LoadSetting = function(self)
@@ -347,6 +325,10 @@ for i, v in ipairs(options) do
 end
 
 
+------------------------------
+-- Profiles
+------------------------------
+
 local defaultProfiles = {}
 
 
@@ -367,7 +349,7 @@ local function getProfiles(db, common, nocurrent)
 		end 
 	end
 	
-	-- add our default profiles to choose from ( or rename existing profiles)
+	-- add our default profiles to choose from (or rename existing profiles)
 	for k, v in pairs(defaultProfiles) do
 		if (common or profiles[k]) and not (nocurrent and k == currentProfile) then
 			profiles[k] = v
@@ -385,17 +367,6 @@ local function getProfiles(db, common, nocurrent)
 	sort(sortProfiles, profileSort)
 	
 	return sortProfiles
-end
-
-
-local function createFontString(parent)
-	local text = parent:CreateFontString(nil, "BACKGROUND", "GameFontHighlightSmall")
-	-- text:SetHeight(32)
-	text:SetPoint("LEFT", 32, 0)
-	text:SetPoint("RIGHT", -32, 0)
-	text:SetJustifyH("LEFT")
-	text:SetJustifyV("TOP")
-	return text
 end
 
 
@@ -522,12 +493,6 @@ local function createProfileUI(name, module)
 	currProfile:SetJustifyV("CENTER")
 	objects.currProfile = currProfile
 
-	-- local chooseDesc = createFontString(frame)
-	-- chooseDesc:SetHeight(32)
-	-- chooseDesc:SetPoint("TOP", reset, "BOTTOM", 0, -8)
-	-- chooseDesc:SetWordWrap(true)
-	-- chooseDesc:SetText(L.choose_desc)
-
 	local newProfile = createEditBox(frame)
 	newProfile:SetWidth(160)
 	newProfile:SetPoint("TOPLEFT", reset, "BOTTOMLEFT", 0, -16)
@@ -551,12 +516,6 @@ local function createProfileUI(name, module)
 	choose.common = true
 	objects.choose = choose
 
-	-- local copyDesc = createFontString(frame)
-	-- copyDesc:SetHeight(32)
-	-- copyDesc:SetPoint("TOP", choose, "BOTTOM", 0, -8)
-	-- copyDesc:SetWordWrap(true)
-	-- copyDesc:SetText(L.copy_desc)
-
 	local copy = createDropDownMenu("MogItCopyProfile"..name, frame, nil, defaultProfiles)
 	copy:SetFrameWidth(144)
 	copy:SetPoint("TOPLEFT", newProfile, "BOTTOMLEFT", 0, -16)
@@ -565,12 +524,6 @@ local function createProfileUI(name, module)
 	copy.func = copyProfileOnClick
 	copy.nocurrent = true
 	objects.copy = copy
-
-	-- local deleteDesc = createFontString(frame)
-	-- deleteDesc:SetHeight(32)
-	-- deleteDesc:SetPoint("TOP", copy, "BOTTOM", 0, -8)
-	-- deleteDesc:SetWordWrap(true)
-	-- deleteDesc:SetText(L.delete_desc)
 
 	local delete = createDropDownMenu("MogItDeleteProfile"..name, frame, nil, defaultProfiles)
 	delete:SetFrameWidth(144)
