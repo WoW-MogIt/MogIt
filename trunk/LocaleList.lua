43224438b2d125825fe44b81d680499532f517fc
aelobin <aelobin@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2011-10-26 03:32:10 +0000

    /

--- trunk/LocaleList.lua
+++ trunk/LocaleList.lua
@@ -5,20 +5,42 @@
 	--[[ Core\Core.xml ]]--
 
 	--[[ Core\Core.lua ]]--
+L["Drop"] = true
+L["Quest"] = true
+L["Vendor"] = true
+L["Crafted"] = true
+L["Achievement"] = true
+L["Code Redemption"] = true
+L["10N"] = true
+L["25N"] = true
+L["10H"] = true
+L["25H"] = true
+L["Select a module"] = true
 L["Toggle Mogit"] = true
+L["Toggle Preview"] = true
 L["Left click to toggle MogIt"] = true
 L["Right click to toggle the preview"] = true
 L["MogIt has loaded! Type \"/mog\" to open it."] = true
 
 	--[[ Core\GUI.lua ]]--
 L["Module"] = true
-L["Select a module"] = true
 L["Base Modules"] = true
 L["Extra Modules"] = true
 L["Sort by"] = true
 L["Preview"] = true
+L["Help"] = true
+L["Rotate"] = true
+L["Left click + horizontal drag"] = true
+L["Zoom"] = true
+L["Left click + vertical drag"] = true
+L["Move"] = true
+L["Right click + drag"] = true
+L["Resize"] = true
+L["Click bottom right corner + drag"] = true
 
 	--[[ Core\Preview.lua ]]--
+L["Save"] = true
+L["Load"] = true
 L["Clear"] = true
 L["Add Item"] = true
 L["Import"] = true
@@ -27,60 +49,92 @@ L["Type the item ID or url in the text box below"] = true
 L["Copy and paste a Wowhead Compare URL into the text box below to import"] = true
 L["http://www.wowhead.com/"] = true
 
+	--[[ Core\Links.lua ]]--
+
 	--[[ Core\URL.lua ]]--
 L["URL"] = true
 L["http://eu.battle.net/wow/en/"] = true
 
-	--[[ Core\Links.lua ]]--
+	--[[ Core\Tooltip.lua ]]--
 
+	--[[ Core\Options.lua ]]--
+L["Hide minimap button"] = true
+L["Catalogue"] = true
+L["No animation"] = true
+L["Dress models"] = true
+L["URL website"] = true
+L["Rows"] = true
+L["Columns"] = true
+L["Tooltip"] = true
+L["Enable tooltip model"] = true
+L["Dress model"] = true
+L["Rotate with mouse wheel"] = true
+L["Auto rotate"] = true
+L["Only transmogrification items"] = true
+L["Only show if modifier is pressed"] = true
+L["Modules"] = true
+L["Wishlist"] = true
+L["Options Profile"] = true
+L["Wishlist Profile"] = true
 
+	--[[ Core\Filters\Filters.xml ]]--
 
-	--[[ Filters\Filters.xml ]]--
-
-	--[[ Filters\Filters.lua ]]--
+	--[[ Core\Filters\Filters.lua ]]--
 L["Results"] = true
 L["No module is selected"] = true
 L["This module has no filters"] = true
 
-	--[[ Filters\level.lua ]]--
+	--[[ Core\Filters\level.lua ]]--
 
-	--[[ Filters\faction.lua ]]--
+	--[[ Core\Filters\faction.lua ]]--
 L["Faction"] = true
 
-	--[[ Filters\class.lua ]]--
+	--[[ Core\Filters\class.lua ]]--
+L["%d selected"] = true
 L["Select All"] = true
 L["Select None"] = true
-L["%d selected"] = true
 
-	--[[ Filters\source.lua ]]--
+	--[[ Core\Filters\source.lua ]]--
 L["Source"] = true
 
-	--[[ Filters\slot.lua ]]--
+	--[[ Core\Filters\slot.lua ]]--
 
-	--[[ Filters\quality.lua ]]--
+	--[[ Core\Filters\quality.lua ]]--
+
+	--[[ Core\Sorting\Sorting.xml ]]--
+
+	--[[ Core\Sorting\Sorting.lua ]]--
+
+	--[[ Core\Sorting\level.lua ]]--
+
+	--[[ Core\Sorting\colour.lua ]]--
+L["Approximate Colour"] = true
 
 
 
 	--[[ Modules\Modules.xml ]]--
 
-	--[[ Modules\Wishlist.lua ]]--
-L["Enter set name"] = true
-L["Wishlist"] = true
-L["Achievement"] = true
+	--[[ Modules\Templates.lua ]]--
+L["Item %d/%d"] = true
 L["Profession"] = true
 L["Slot"] = true
 
+	--[[ Modules\Wishlist.lua ]]--
+L["Enter set name"] = true
+
 	--[[ Modules\Base.lua ]]--
-L["Drop"] = true
-L["Quest"] = true
-L["Vendor"] = true
-L["Crafted"] = true
-L["Code Redemption"] = true
-L["10N"] = true
-L["25N"] = true
-L["10H"] = true
-L["25H"] = true
 L["Click to load addon"] = true
-L["Item %d/%d"] = true
+L["Change item"] = true
+L["Left click"] = true
+L["Chat link"] = true
+L["Shift + Left click"] = true
+L["Try on"] = true
+L["Ctrl + Left click"] = true
+L["Add to wishlist"] = true
+L["Right click"] = true
+L["Item URL"] = true
+L["Shift + Right click"] = true
+L["Add to preview"] = true
+L["Ctrl + Right click"] = true
 
 	--[[ Modules\Extra.lua ]]--
