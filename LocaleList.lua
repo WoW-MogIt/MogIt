4f74e4dcb3e96604efa5b7e24deecd86b648f557
aelobin <28518245+Aelobin@users.noreply.github.com>
2012-09-03 22:03:53 +0000

    /

--- LocaleList.lua
+++ LocaleList.lua
@@ -1,63 +1,69 @@
-
+L["Transmogrification Assistant"] = true
 
 
 
 	--[[ Core\Core.xml ]]--
 
 	--[[ Core\Core.lua ]]--
-L["Drop"] = true
-L["Quest"] = true
-L["Vendor"] = true
-L["Crafted"] = true
-L["Achievement"] = true
-L["Code Redemption"] = true
-L["10N"] = true
-L["25N"] = true
-L["10H"] = true
-L["25H"] = true
-L["Select a module"] = true
 L["Toggle Mogit"] = true
 L["Toggle Preview"] = true
 L["Left click to toggle MogIt"] = true
 L["Right click to toggle the preview"] = true
+L["The \124cFFFFFFFF%s\124r module is already loaded."] = true
+L["The \124cFFFFFFFF%s\124r module needs to be updated to work with this version of MogIt."] = true
+L["The \124cFFFFFFFF%s\124r module requires you to update MogIt for it to work."] = true
 L["MogIt has loaded! Type \"/mog\" to open it."] = true
 
 	--[[ Core\GUI.lua ]]--
-L["Module"] = true
+L["Modules"] = true
 L["Base Modules"] = true
 L["Extra Modules"] = true
-L["Sort by"] = true
 L["Preview"] = true
+L["Equipped"] = true
+L["Catalogue"] = true
+L["Hide Filters"] = true
+L["Show Filters"] = true
+L["Sorting"] = true
+L["Dress models"] = true
+L["New Preview"] = true
+L["Hide Previews"] = true
+L["Show Previews"] = true
+L["Synchronize Positioning"] = true
+L["Options"] = true
 L["Help"] = true
-L["Rotate"] = true
-L["Left click + horizontal drag"] = true
-L["Zoom"] = true
-L["Left click + vertical drag"] = true
-L["Move"] = true
-L["Right click + drag"] = true
-L["Resize"] = true
-L["Click bottom right corner + drag"] = true
-L["Chat link"] = true
-L["Shift + Left click"] = true
-L["Try on"] = true
-L["Ctrl + Left click"] = true
-L["Wishlist menu"] = true
-L["Right click"] = true
-L["Item URL"] = true
-L["Shift + Right click"] = true
-L["Remove from preview"] = true
-L["Ctrl + Right click"] = true
+L["How to use"] = true
+L["Model controls"] = true
+L["Left click and drag horizontally to rotate"] = true
+L["Left click and drag vertically to zoom"] = true
+L["Right click and drag to move"] = true
+L["Module controls"] = true
 
 	--[[ Core\Preview.lua ]]--
-L["Save"] = true
-L["Load"] = true
-L["Clear"] = true
+L["Gender"] = true
 L["Add Item"] = true
-L["Import"] = true
 L["Chat Link"] = true
+L["Import / Export"] = true
+L["Clear"] = true
+L["New set"] = true
+L["Other profiles"] = true
+L["Basic Controls"] = true
+L["Click the bottom right corner and drag to resize"] = true
+L["Click the \"Activate\" button to set this as the active preview"] = true
+L["Slot Controls"] = true
+L["Shift + Left click to link an item to chat"] = true
+L["Ctrl + Left click to try on an item"] = true
+L["Right click to show the item menu"] = true
+L["Shift + Right click to show a URL for the item"] = true
+L["Ctrl + Right click to remove the item from the preview"] = true
+L["Load"] = true
+L["Save"] = true
+L["Preview %d"] = true
+L["Activate"] = true
 L["Type the item ID or url in the text box below"] = true
 L["Copy and paste a Wowhead Compare URL into the text box below to import"] = true
+L["Import"] = true
 L["http://www.wowhead.com/"] = true
+L["Are you sure you want to close this set?"] = true
 
 	--[[ Core\Links.lua ]]--
 
@@ -66,12 +72,11 @@ L["URL"] = true
 L["http://eu.battle.net/wow/en/"] = true
 
 	--[[ Core\Tooltip.lua ]]--
+L["This item is on your wishlist."] = true
 
 	--[[ Core\Options.lua ]]--
 L["Hide minimap button"] = true
-L["Catalogue"] = true
 L["No animation"] = true
-L["Dress models"] = true
 L["URL website"] = true
 L["Rows"] = true
 L["Columns"] = true
@@ -82,7 +87,6 @@ L["Rotate with mouse wheel"] = true
 L["Auto rotate"] = true
 L["Only transmogrification items"] = true
 L["Only show if modifier is pressed"] = true
-L["Modules"] = true
 L["Wishlist"] = true
 L["Options Profile"] = true
 L["Wishlist Profile"] = true
@@ -94,7 +98,11 @@ L["Results"] = true
 L["No module is selected"] = true
 L["This module has no filters"] = true
 
+	--[[ Core\Filters\name.lua ]]--
+
 	--[[ Core\Filters\level.lua ]]--
+
+	--[[ Core\Filters\itemLevel.lua ]]--
 
 	--[[ Core\Filters\faction.lua ]]--
 L["Faction"] = true
@@ -111,6 +119,9 @@ L["Source"] = true
 
 	--[[ Core\Filters\quality.lua ]]--
 
+	--[[ Core\Filters\bind.lua ]]--
+L["Bind"] = true
+
 	--[[ Core\Sorting\Sorting.xml ]]--
 
 	--[[ Core\Sorting\Sorting.lua ]]--
@@ -120,19 +131,34 @@ L["Source"] = true
 	--[[ Core\Sorting\colour.lua ]]--
 L["Approximate Colour"] = true
 
+	--[[ Core\Sorting\display.lua ]]--
+L["Display ID"] = true
+
 
 
 	--[[ Modules\Modules.xml ]]--
 
 	--[[ Modules\Templates.lua ]]--
+L["New preview"] = true
+L["Active preview"] = true
+L["Add to wishlist"] = true
+L["Add to set"] = true
+L["Delete"] = true
 L["Item %d/%d"] = true
-L["Profession"] = true
 L["Slot"] = true
+L["You have this item."] = true
+L["Load module to see other items using this appearance."] = true
+L["Other items using this appearance:"] = true
+L["Add set to wishlist"] = true
+L["Rename set"] = true
+L["Delete set"] = true
 
 	--[[ Modules\Wishlist.lua ]]--
-L["Change item"] = true
-L["Left click"] = true
-L["Add to preview"] = true
+L["Right click for additional options"] = true
+L["Shift-left click to link"] = true
+L["Shift-right click for item URL"] = true
+L["Ctrl-left click to try on in dressing room"] = true
+L["Ctrl-right click to preview with MogIt"] = true
 L["Enter set name"] = true
 L["Enter new set name"] = true
 L["Delete set '%s'?"] = true
@@ -140,5 +166,8 @@ L["Overwrite set '%s'?"] = true
 
 	--[[ Modules\Base.lua ]]--
 L["Click to load addon"] = true
+L["Left click to cycle through items"] = true
 
 	--[[ Modules\Extra.lua ]]--
+
+	--[[ Modules\Tokens.lua ]]--
