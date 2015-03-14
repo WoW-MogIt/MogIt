92ff86bc568aa56558f01e072190233584ad0a7f
aelobin <aelobin@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2015-03-14 13:16:52 +0000

    /

---
+++ trunk/Modules/MogIt_Cosmetic/Chest.lua
@@ -0,0 +1,4 @@
+local i=MogIt.base.AddSlot("Chest",...)
+i(105743,nil,126296,4,nil,nil,64,3,nil,1,nil,nil,nil)
+i(117408,nil,137990,3,90,1,nil,4,nil,4,nil,991,nil)
+i(118367,nil,137284,3,90,2,nil,4,nil,4,nil,990,nil)
