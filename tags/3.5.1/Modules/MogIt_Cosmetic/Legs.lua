79d9c0abc5a28899e5350804a4e97c9ef7cdcd6f
lombra <lombra@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2015-04-29 23:07:31 +0000

    Tagging as 3.5.1.

---
+++ tags/3.5.1/Modules/MogIt_Cosmetic/Legs.lua
@@ -0,0 +1,4 @@
+local i=MogIt.base.AddSlot("Legs",...)
+i(105746,nil,124865,4,nil,nil,64,3,nil,1,nil,nil,nil)
+i(117411,nil,137975,3,90,1,nil,4,nil,4,nil,991,nil)
+i(118370,nil,137282,3,90,2,nil,4,nil,4,nil,990,nil)
