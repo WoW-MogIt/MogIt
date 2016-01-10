728865296d4f7f4fb9c1054f1d90304a83d2dac4
lombra <lombra@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2016-01-10 03:12:07 +0000

    Tagging as 3.5.7.

---
+++ tags/3.5.7/Modules/MogIt_Cosmetic/Chest.lua
@@ -0,0 +1,6 @@
+local i=MogIt.base.AddSlot("Chest",...)
+i(105743,nil,126296,4,nil,nil,64,3,nil,1,nil,nil,nil)
+i(116052,nil,141551,1,1,nil,nil,nil,nil,4,nil,823,nil)
+i(116137,nil,141554,1,1,nil,nil,nil,nil,4,nil,823,nil)
+i(117408,nil,137990,3,90,1,nil,4,nil,4,nil,991,nil)
+i(118367,nil,137284,3,90,2,nil,4,nil,4,nil,990,nil)
