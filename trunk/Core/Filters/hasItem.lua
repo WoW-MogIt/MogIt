da103bf5b27e92ce2b1a019f3f296e5eaf6ca4ae
Lombra <Lombra@dbbbf036-dbfb-404a-a7be-5d438c3d3665>
2018-12-24 00:09:22 +0000

    - Add Not collected filter.

--- trunk/Core/Filters/hasItem.lua
+++ trunk/Core/Filters/hasItem.lua
@@ -2,7 +2,7 @@ local MogIt,mog = ...;
 local L = mog.L;
 
 local f = mog:CreateFilter("hasItem");
-local enabled;
+local collected, notCollected;
 
 f:SetHeight(41);
 
@@ -13,23 +13,37 @@ f.label:SetText(L["Owned items"]..":");
 f.label:SetJustifyH("LEFT");
 
 f.hasItem = CreateFrame("CheckButton",nil,f,"UICheckButtonTemplate");
-f.hasItem.text:SetText(L["Only items you own"]);
+f.hasItem.text:SetText(L["Items you have collected"]);
 f.hasItem:SetPoint("TOPLEFT",f.label,"BOTTOMLEFT");
 f.hasItem:SetScript("OnClick",function(self)
-	enabled = self:GetChecked();
+	collected = self:GetChecked();
+	mog:BuildList();
+end);
+
+f.hasNotItem = CreateFrame("CheckButton",nil,f,"UICheckButtonTemplate");
+f.hasNotItem.text:SetText(L["Items you have not collected"]);
+f.hasNotItem:SetPoint("TOPLEFT",f.hasItem,"BOTTOMLEFT");
+f.hasNotItem:SetScript("OnClick",function(self)
+	notCollected = self:GetChecked();
 	mog:BuildList();
 end);
 
 function f.Filter(item)
-	if not enabled then
+	if collected and notCollected then
 		return true
 	end
+	if not collected and not notCollected then
+		return false
+	end
 	if type(item) == "table" then return end
-	return mog:HasItem(item);
+	if collected then return mog:HasItem(item); end
+	if notCollected then return not mog:HasItem(item); end
 end
 
 function f.Default()
-	f.hasItem:SetChecked(false);
-	enabled = false;
+	f.hasItem:SetChecked(true);
+	f.hasNotItem:SetChecked(true);
+	collected = true;
+	notCollected = true;
 end
 f.Default();
